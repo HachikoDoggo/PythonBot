a81d1dc96867238040926fe1922e06c333fb8544
HachikoDoggo <56903191+HachikoDoggo@users.noreply.github.com>
2019-10-23 13:19:45 +1030

    Add files via upload

--- PythonBot/PythonBot.py
+++ PythonBot/PythonBot.py
@@ -23,12 +23,10 @@ handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(me
 logger.addHandler(handler)
 
 status = cycle(['Serving 1 Server', 'And thats my testing server', 'LOL', 'Contact My creator on xbox!', 'XBOX=Hachi Is My Dog', 'Remember im still in development!', 'FUCK LIFE'])
-#valid_users = ["╲⎝⧹Tony Stark⧸⎠╱#6947"]
+
 
 client = commands.Bot(command_prefix = '.')
 
-#joined = 0
-#messages = 0
 
 @client.event
 async def on_ready():
@@ -39,16 +37,6 @@ async def on_ready():
 @tasks.loop(seconds=3)
 async def change_status():
 	await client.change_presence(activity=discord.Game(next(status)))
-
-@client.event
-async def on_message(message):
-    id = client.get_guild(596539671471980544)
-
-    if message.content.find("!hello") != -1:
-        await message.channel.send("Hi")
-    elif message.content == "!users":
-        await message.channel.send(f"""# of Members: {id.member_count}""")
-
 
 @client.event
 async def update_stats():
@@ -62,13 +50,9 @@ async def load(ctx, extension):
 @client.command()
 async def unload(ctx, extension):
 		client.unload_extension(f'cogs.{extension}')
+
 for filename in os.listdir(r"C:\Users\missw\Desktop\PythonBot\Cogs"):
 	if filename.endswith('.py'):
 		client.load_extension(f'cogs.{filename[:-3]}')
-
-@client.command()
-async def say(ctx, arg):
-	await ctx.send(arg)
-
 
 client.run(token)
